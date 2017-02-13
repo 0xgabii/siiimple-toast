48be43fded2864769f811e172f28ee9913d2acf0
gomonk3037 <gomonk3037@gmail.com>
2017-02-13 12:33:19 +0900

    add 6 direction

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 class siiimpleToast {
   constructor(settings) {
-    if (!settings.rootElement) throw new Error('Please set parameter "rootElement" ');
+    // default settings
+    if (!settings) {
+      settings = {
+        vertical: 'top',
+        horizontal: 'center'
+      }
+    }
+    if (!settings.vertical) throw new Error('Please set parameter "vertical" ex) bottom, top ');
+    if (!settings.horizontal) throw new Error('Please set parameter "horizontal" ex) left, center, right ');
     this._settings = settings;
+    this.defaultClass = 'siiimpleToast';
     this.defaultStyle = {
       position: 'fixed',
-      left: '50%',
-      top: '-100px',
       padding: '1rem 1.2rem',
-      minWidth: '15rem',
+      minWidth: '17rem',
       zIndex: '10',
       borderRadius: '2px',
       color: 'white',
@@ -16,56 +23,85 @@ class siiimpleToast {
       pointerEvents: 'none',
       opacity: 0,
       boxShadow: '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
-      transform: 'translateX(-50%) scale(0.5)',
+      transform: 'scale(0.5)',
       transition: 'all 0.4s ease-out'
     }
+    // verticalStyle
+    if (this._settings.vertical == 'bottom')
+      this.verticalStyle = { bottom: '-100px' };
+    else if (this._settings.vertical == 'top')
+      this.verticalStyle = { top: '-100px' };
+    // horizontalStyle
+    if (this._settings.horizontal == 'left')
+      this.horizontalStyle = { left: '1rem' };
+    else if (this._settings.horizontal == 'center')
+      this.horizontalStyle = { left: '50%', transform: 'translateX(-50%) scale(0.5)' };
+    else if (this._settings.horizontal == 'right')
+      this.horizontalStyle = { right: '1rem' };
   }
-  logic(state, message) {
-    const root = document.querySelector(this._settings.rootElement),
+  init(state, message) {
+    const root = document.querySelector('body'),
       newToast = document.createElement('div');
 
-    let time = 0;
-
     // set Common class
-    newToast.className = 'siiimpleToast';
+    newToast.className = this.defaultClass;
     // set message
     newToast.innerHTML = message;
-    // set Style
+    // set defaultStyle
     Object.assign(newToast.style, this.defaultStyle);
+    // set message Mode
     if (state == 'default')
       newToast.style.backgroundColor = '#323232';
     else if (state == 'alert')
       newToast.style.backgroundColor = '#d93737';
     else if (state == 'success')
       newToast.style.backgroundColor = '#8BC34A';
+    // set vertical direction
+    Object.assign(newToast.style, this.verticalStyle);
+    // set horizontal direction
+    Object.assign(newToast.style, this.horizontalStyle);
 
     // insert Toast DOM
     root.insertBefore(newToast, root.firstChild);
 
+    let time = 0;
+
     // add Action
     setTimeout(() => {
       let stackMargin = 15;
-      const toast = document.querySelectorAll('.siiimpleToast');
+      const toast = document.getElementsByClassName(this.defaultClass);
 
       newToast.style.opacity = 1;
-      newToast.style.transform = 'translateX(-50%) scale(1)';
+      if (this._settings.horizontal == 'center')
+        newToast.style.transform = 'translateX(-50%) scale(1)';
+      else
+        newToast.style.transform = 'scale(1)';
 
       for (let i = 0; i < toast.length; i++) {
         let height = toast[i].offsetHeight;
         let topMargin = 15;
-        toast[i].style.top = stackMargin + 'px';
+
+        if (this._settings.vertical == 'bottom')
+          toast[i].style.bottom = stackMargin + 'px';
+        else if (this._settings.vertical == 'top')
+          toast[i].style.top = stackMargin + 'px';
+
         stackMargin += height + topMargin;
       }
     }, time += 100);
 
     // remove Action
     setTimeout(() => {
-      let winWidth = window.outerWidth;
-      let width = newToast.offsetWidth;
+      let coordinate_left = newToast.getBoundingClientRect().left,
+        coordinate_right = newToast.getBoundingClientRect().right,
+        width = newToast.offsetWidth;
+
+      if (this._settings.horizontal == 'right')
+        newToast.style.right = '-' + width + 'px';
+      else
+        newToast.style.left = coordinate_left + width + 'px';
 
       newToast.style.opacity = 0;
-      newToast.style.left = (winWidth / 2) + width + 'px';
-
     }, time += 3000);
 
     // delete Dom
@@ -73,15 +109,14 @@ class siiimpleToast {
       const parent = newToast.parentNode;
       parent.removeChild(newToast);
     }, time += 500);
-
   }
   message(message) {
-    this.logic('default', message);
+    this.init('default', message);
   }
   success(message) {
-    this.logic('success', message);
+    this.init('success', message);
   }
   alert(message) {
-    this.logic('alert', message);
+    this.init('alert', message);
   }
 }
