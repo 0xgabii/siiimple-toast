d8992d44441464a8074f5fa8220b6c446fbd3b2d
Go Eun-Seok <gomonk3037@gmail.com>
2017-06-14 23:12:13 +0900

    add rules & golbals

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "globals": {
+        "document": true
+    },
+    "rules": {
+        "no-param-reassign": 0,
+        "class-methods-use-this": 0,
+    }
 };
